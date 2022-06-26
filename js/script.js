31b9c6fead96d10db35e6d2e5eb5fc97a13bd080
Josh Warner <joshua.ryan.warner@icloud.com>
2022-06-26 15:53:35 +1000

    menu animation

--- js/script.js
+++ js/script.js
@@ -2,15 +2,20 @@ $(document).ready(function () {
 
     $(".menu").hide();
     $(".menuFilter").hide();
+    $(".menu-list li").hide();
     
-    $( "header .menu-icon" ).click(function() {
-        $( ".menu" ).fadeIn(300);
-        $( ".menuFilter" ).fadeIn(300);
+    $( "header .open" ).click(function() {
+        $( ".menu" ).fadeIn(400);
+        $( ".menuFilter" ).fadeIn(400);
+        $(".menu-list li").delay(200).each(function(i) {
+            $(this).delay(100 * i).fadeIn(700);
+        });
     });
     
-    $( ".menuFilter" ).click(function() {
-        $( ".menu" ).fadeOut(300);
-        $( ".menuFilter" ).fadeOut(300);
+    $( "header .close" ).click(function() {
+        $( ".menu" ).fadeOut(400);
+        $( ".menuFilter" ).fadeOut(400);
+        $(".menu-list li").fadeOut(400);
     });
     
 });
