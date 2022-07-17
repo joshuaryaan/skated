94e3a62e43325ec1ee4076d58f0a8770a460930b
Josh Warner <joshua.ryan.warner@icloud.com>
2022-07-17 13:10:45 +1000

    background colour test

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 $(document).ready(function () { 
 
     $(".menu").hide();
-    $(".menuFilter").hide();
     $(".menu-list li").hide();
     
     $( "header .open" ).click(function() {
         $( ".menu" ).fadeIn(400);
-        $( ".menuFilter" ).fadeIn(400);
         $(".menu-list li").delay(200).each(function(i) {
             $(this).delay(100 * i).fadeIn(700);
         });
@@ -18,7 +16,6 @@ $(document).ready(function () {
     
     $( "header .close" ).click(function() {
         $( ".menu" ).fadeOut(400);
-        $( ".menuFilter" ).fadeOut(400);
         $(".menu-list li").fadeOut(400);
         $('html, body').css({
             overflow: 'auto',
